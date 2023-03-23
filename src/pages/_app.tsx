09b2034011ba4bdef8e5543ff0c263507be2cb4c
Mirko kawa <mirkokawa7@gmail.com>
2023-03-23 07:22:07 +0300

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import '../styles/index.css'
+import Layout from '../components/Layout'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>NextJS TailwindCSS TypeScript Starter</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
+}
+
+export default MyApp
